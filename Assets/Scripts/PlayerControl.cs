ea54ecae03969c90808ad98410b009e97125a46a
GAVING0TGAME423 <78512752+GAVING0TGAME423@users.noreply.github.com>
2021-03-19 23:41:22 -0700

    Initial commit

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    private Rigidbody rbplayer;
+    public float GravityModifier;
+    public float JumpForce;
+    private bool onground = true;
+    void Start()
+    {
+       rbplayer = GetComponent<Rigidbody>();
+        Physics.gravity *= GravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool spacedown = Input.GetKeyDown(KeyCode.Space);
+        if (spacedown && onground)
+        {
+            rbplayer.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
+            onground = false;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        onground = true;
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject ObstaclePrefab;
+    private Vector3 spawnpoint = new Vector3(20, 0, 0);
+    void Start()
+    {
+        InvokeRepeating("Spawn", 1, 2);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Spawn()
+    {
+        Instantiate(ObstaclePrefab, spawnpoint, ObstaclePrefab.transform.rotation);
+    }
+}
